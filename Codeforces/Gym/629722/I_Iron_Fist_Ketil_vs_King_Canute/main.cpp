e9790633b10507122078c595b95de8530b7c1b1d
zum04 <zum110806@gmail.com>
2025-10-21 23:09:12 -0600

    Solución: Codeforces 629722I - Iron_Fist_Ketil_vs_King_Canute

---
+++ Codeforces/Gym/629722/I_Iron_Fist_Ketil_vs_King_Canute/main.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main(){
+    int n, m, k;
+    cin >> n >> m >> k;
+    if(n/k >= m) cout << "Iron fist Ketil";
+    else cout << "King Canute";
+}
